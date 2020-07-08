090b5937820f354d33c4fb5b32d65821e303f50f
PC <nareremi@gmail.com>
2020-07-08 21:34:32 +0000

    Elaboration de l'activité de connection (design) Elaboration de l'activité de connection (design)

--- app/src/main/java/apps/coders4togo/hhaya/Conn_activity.java
+++ app/src/main/java/apps/coders4togo/hhaya/Conn_activity.java
@@ -2,13 +2,30 @@ package apps.coders4togo.hhaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Conn_activity extends AppCompatActivity {
+
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.conn_activity);
+
+        floatingActionButton = findViewById(R.id.btRegister);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Conn_activity.this, Insc_activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
