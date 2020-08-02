29045e42b9874a18759e5f63de2c53ff65c7f84e
PC <nareremi@gmail.com>
2020-08-02 12:41:01 +0000

    Résolution de quelques problèmes

--- app/src/main/java/apps/coders4togo/hhaya/Insc_activity.java
+++ app/src/main/java/apps/coders4togo/hhaya/Insc_activity.java
@@ -2,13 +2,29 @@ package apps.coders4togo.hhaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Insc_activity extends AppCompatActivity {
+
+    private Button test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.insc_activity);
+
+        test = findViewById(R.id.connet);
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Insc_activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
